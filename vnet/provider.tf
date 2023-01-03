85237d619892f62fe42a6bfc7879c7c045bc297c
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-01-03 23:50:30 +0530

    Update provider.tf

--- vnet/provider.tf
+++ vnet/provider.tf
@@ -4,8 +4,8 @@ terraform {
     storage_account_name = "tfstatedemo1"
     container_name       = "tfstate"
     key                  = "vnet.tfstate"
-    use_oidc = true
-    use_azuread_auth = true
+#     use_oidc = true
+#     use_azuread_auth = true
     client_id = "0cffc591-20bd-42b4-b2f5-292ba2b04a1d"
     tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
     subscription_id = "7b5c7d11-8bc3-4105-9c6f-41222b38b95f"
@@ -27,9 +27,8 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-#    use_oidc = true
-  }
+  use_oidc = true
+  features {}
 }
 
 provider "azuread" {}
