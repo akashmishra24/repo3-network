61329680a3328305836fc5558c041d9be3ef1060
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-01-04 00:39:06 +0530

    Update provider.tf

--- vnet/provider.tf
+++ vnet/provider.tf
@@ -23,10 +23,10 @@ terraform {
 }
 
 provider "azurerm" {
-  use_msi = true
+  use_oidc = true
   tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
   subscription_id = "7b5c7d11-8bc3-4105-9c6f-41222b38b95f"
-  client_id = "a7f86a9f-b804-4844-8980-652cd145b25b"
+  client_id = "0cffc591-20bd-42b4-b2f5-292ba2b04a1d"
   features {}
 }
 
