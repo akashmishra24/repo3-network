ed4e5137135d12caac468b7f1ea88a4eea9f0e69
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-01-04 00:15:20 +0530

    Update provider.tf

--- vnet/provider.tf
+++ vnet/provider.tf
@@ -1,9 +1,11 @@
 terraform {
   backend "azurerm" {
-#     resource_group_name  = "rg-iac-cox-poc-01"
-#     storage_account_name = "tfstatedemo1"
-#     container_name       = "tfstate"
+    resource_group_name  = "rg-iac-cox-poc-01"
+    storage_account_name = "tfstatedemo1"
+    container_name       = "tfstate"
     key                  = "vnet.tfstate"
+    tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+    subscription_id = "7b5c7d11-8bc3-4105-9c6f-41222b38b95f"
 #     use_azuread_auth = true
   }
 }
@@ -23,10 +25,7 @@ terraform {
 }
 
 provider "azurerm" {
-  use_oidc = true
-  client_id = "0cffc591-20bd-42b4-b2f5-292ba2b04a1d"
-  tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-  subscription_id = "7b5c7d11-8bc3-4105-9c6f-41222b38b95f"
+  use_msi = true
   features {}
 }
 
