796bcffbd9f2a392a9981dd54382dee0e17c02c5
akashmishra24 <89220556+akashmishra24@users.noreply.github.com>
2023-01-03 19:55:02 +0530

    Update provider.tf

--- vnet/provider.tf
+++ vnet/provider.tf
@@ -7,6 +7,7 @@ terraform {
     use_oidc = true
     client_id = "0cffc591-20bd-42b4-b2f5-292ba2b04a1d"
     tenant_id = "72f988bf-86f1-41af-91ab-2d7cd011db47"
+    subscription_id = "7b5c7d11-8bc3-4105-9c6f-41222b38b95f"
   }
 }
 
